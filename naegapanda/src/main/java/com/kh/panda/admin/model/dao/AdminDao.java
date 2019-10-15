6f3234138d79e1c43b07af9d7d6288efcc72dd99
tntnfdj <tntnfdj@naver.com>
2019-10-15 10:40:09 +0900

    Admin 초기 작업
    Admin 초기 설계
    (controller model.vo .dao .service
    mapper view)

---
+++ naegapanda/src/main/java/com/kh/panda/admin/model/dao/AdminDao.java
@@ -0,0 +1,61 @@
+package com.kh.panda.admin.model.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("aDao")
+public class AdminDao {
+
+	
+//------------------------------vv공통vv-------------------------------//
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+//------------------------------^^공통^^------------------------------//
+	
+	
+	
+	
+	
+	
+	
+	
+//------------------------------vv수진vv------------------------------//
+	
+	
+	
+	
+	
+	
+	
+	
+//------------------------------^^수진^^------------------------------//
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//------------------------------vv명지vv------------------------------//
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//------------------------------^^명지^^------------------------------//
+	
+	
+	
+	
+	
+}
