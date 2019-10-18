d7107a7b357c1adf64c0be73daa1ab82b034add0
koo-95 <aaudwl9534@gmail.com>
2019-10-18 12:04:28 +0900

    .

--- naegapanda/src/main/java/com/kh/panda/myShopping/payment/model/service/PaymentServiceImpl.java
+++ naegapanda/src/main/java/com/kh/panda/myShopping/payment/model/service/PaymentServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.panda.myShopping.basket.model.dao.BasketDao;
+import com.kh.panda.myShopping.basket.model.vo.Basket;
 import com.kh.panda.myShopping.payment.model.dao.PaymentDao;
 import com.kh.panda.myShopping.payment.model.vo.Payment;
 
@@ -13,6 +15,8 @@ public class PaymentServiceImpl implements PaymentService{
 
 	@Autowired
 	private PaymentDao paDao;
+	@Autowired
+	private BasketDao baDao;
 	
 	@Override
 	public ArrayList<Payment> myPaymentList(int mNo) {
@@ -21,8 +25,30 @@ public class PaymentServiceImpl implements PaymentService{
 
 	@Override
 	public int addPayment(Payment p) {
-		// 장바구니 삭제
-		return paDao.addPayment(p);
+
+		// 현재 내 장바구니 가져옴
+		ArrayList<Basket> list = baDao.selectbasketList(p.getmNo());
+		
+		int count = 0;
+		
+		// 장바구니에 담긴 각 상품을 결제 태이블에 등록하고 장바구니에서는 삭제하는 로직
+		for(Basket b : list) {
+			
+			Payment addP = p; // input으로 받은 결재정보 복사
+			addP.setpId(b.getpId());
+			addP.setCount(b.getAmount());
+			
+			int result = paDao.addPayment(addP);
+			int result2 = baDao.deleteBasket(b);
+			
+			if(result == 0 || result2 == 0) {
+				return 0;
+			}else {
+				count++;
+			}
+		}
+		
+		return count;
 	}
 
 	@Override
