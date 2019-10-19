2c3e2308937a83e6a9e4223088c4668dd930f1a2
koo-95 <aaudwl9534@gmail.com>
2019-10-19 17:43:08 +0900

    ;

--- naegapanda/src/main/java/com/kh/panda/admin/board/qna/model/dao/QnaDao.java
+++ naegapanda/src/main/java/com/kh/panda/admin/board/qna/model/dao/QnaDao.java
@@ -1,14 +1,33 @@
 package com.kh.panda.admin.board.qna.model.dao;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.kh.panda.admin.board.qna.model.vo.Qna;
+import com.kh.panda.common.PageInfo;
 
 @Repository("qDao")
 public class QnaDao {
 
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+	
+	public int getListCount() {
+		return sqlSession.selectOne("qnaMapper.getListCount");
+	}
+	
+	public ArrayList<Qna> selectList(PageInfo pi){
+		
+		// offset = 몇개의 게시글을 건너띄고 조회할 건지
+		int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit();
+		RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
+		
+		return (ArrayList)sqlSession.selectList("qnaMapper.selectList", null, rowBounds);
+	}
 	
 	
 }
